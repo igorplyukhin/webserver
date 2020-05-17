9a220d76e26979f257c05d1fd7d620a507f6e6bb
Igor <51264207+igorplyukhin@users.noreply.github.com>
2020-05-18 00:34:41 +0500

    POST_request_handle

--- req_handler.py
+++ req_handler.py
@@ -2,18 +2,37 @@
 import mimetypes
 from req_parser import HTTPError
 import os
+from pathlib import Path
 
 
 def handle_request(server, request):
     if request.method == 'GET':
         return handle_get_request(server, request)
     if request.method == 'POST':
-        return handle_post_request(server,request)
+        return handle_post_request(server, request)
     else:
         raise NotImplementedError()
 
-def handle_post_request(server,request):
-    pass
+
+def handle_post_request(server, request):
+    file_path = f'{server.root_directory}{request.path}'
+    if os.path.exists(file_path) and os.path.isfile(file_path):
+        file_size = os.path.getsize(file_path)
+        file_content = read_file(server, file_path, file_size)
+        if file_content == request.body:
+            return Response(304, 'Not Modified', {'Connection': define_connection_type(request)})
+        else:
+            rewrite_file(server, file_path, request.body)
+            return Response(200, 'OK', {'Connection': define_connection_type(request)})
+    else:
+        dir_name = os.path.dirname(file_path)
+        Path(dir_name).mkdir(parents=True, exist_ok=True)
+        os.mknod(file_path)
+        if os.path.isfile(file_path):
+            rewrite_file(server, file_path, request.body)
+            return Response(201, 'Created', {'Connection': define_connection_type(request)})
+        return Response(304, 'Not Modified', {'Connection': define_connection_type(request)})
+
 
 def handle_get_request(server, request):
     file_path = f'{server.root_directory}{request.path}'
@@ -28,29 +47,16 @@ def handle_get_request(server, request):
 
 def handle_get_file(server, request, file_path):
     try:
-        if file_path in server.fd_cache:
-            fd = server.fd_cache[file_path]
-        else:
-            if hasattr(os, 'O_BINARY'): # Windows
-                attributes = os.O_RDONLY | os.O_BINARY
-            else:
-                attributes = os.O_RDONLY
-            server.fd_cache[file_path] = os.open(file_path, attributes)
-            fd = server.fd_cache[file_path]
-
         file_size = os.path.getsize(file_path)
-        file_content = os.read(fd, file_size)
-        os.lseek(fd, 0, 0)
+        file_content = read_file(server, file_path, file_size)
     except (FileNotFoundError, IsADirectoryError):
         raise HTTPError(404, "Not Found")
 
     headers = {'Server': 'my_server',
                'Content-Type': mimetypes.guess_type(request.path)[0],
-               'Content-Length': file_size}
-    if 'Connection' in request.headers:
-        headers['Connection'] = request.headers['Connection']
-    else:
-        headers['Connection'] = 'close'
+               'Content-Length': file_size,
+               'Connection': define_connection_type(request)}
+
     return Response(200, 'OK', headers, file_content)
 
 
@@ -58,11 +64,9 @@ def handle_get_dir(request, file_path):
     content = os.popen(f"cd {file_path}/ && tree -H '.' -L 1 --noreport --charset utf-8").read()
     headers = {'Server': 'my_server',
                'Content-Type': 'text/html',
-               'Content-Length': len(content)}
-    if 'Connection' in request.headers:
-        headers['Connection'] = request.headers['Connection']
-    else:
-        headers['Connection'] = 'close'
+               'Content-Length': len(content),
+               'Connection': define_connection_type(request)}
+
     return Response(200, 'OK', headers, content.encode('utf-8'))
 
 
@@ -77,6 +81,38 @@ def handle_error(error):
         body = b'Internal Server Error'
     return Response(status, description, {'Content-Length': len(body), 'Connection': 'keep-alive'}, body)
 
+
+def rewrite_file(server, file_path, data_to_write):
+    fd = get_file_descriptor(server, file_path)
+    os.truncate(fd, 0)
+    os.write(fd, data_to_write)
+    os.lseek(fd, 0, 0)
+
+
+def define_connection_type(request):
+    if 'Connection' in request.headers:
+        return request.headers['Connection']
+    return 'close'
+
+
+def read_file(server, file_path, file_size):
+    fd = get_file_descriptor(server, file_path)
+    file_content = os.read(fd, file_size)
+    os.lseek(fd, 0, 0)
+    return file_content
+
+
+def get_file_descriptor(server, file_path):
+    if file_path in server.fd_cache:
+        fd = server.fd_cache[file_path]
+    else:
+        if hasattr(os, 'O_BINARY'):  # Windows
+            attributes = os.O_RDWR | os.O_BINARY
+        else:
+            attributes = os.O_RDWR
+        server.fd_cache[file_path] = os.open(file_path, attributes)
+        fd = server.fd_cache[file_path]
+    return fd
 
 class Response:
     def __init__(self, status, description, headers=None, body=None):
