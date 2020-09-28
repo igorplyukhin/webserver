fdeef761304d9a407c6d17fcb67fd4cc362d2dd1
Igor Plyukhin <51264207+igorplyukhin@users.noreply.github.com>
2020-09-28 17:45:22 +0500

    bandwidth limit

--- resp_sender.py
+++ resp_sender.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3.8
+import asyncio
 
 
-def send_response(writer, resp):
+async def send_response(server, writer, resp):
     status_line = f'HTTP/1.1 {resp.status} {resp.description}\r\n'
     writer.write(status_line.encode('iso-8859-1'))
 
@@ -11,7 +12,17 @@ def send_response(writer, resp):
 
     writer.write(b'\r\n')
     if resp.body:
-        writer.write(resp.body)
+        if server.bandwidth:
+            await sleep_write(writer, resp.body, server.bandwidth)
+        else:
+            writer.write(resp.body)
 
     if resp.headers is None or resp.headers.get('Connection') != 'keep-alive':
         raise ConnectionResetError
+
+
+async def sleep_write(writer, data, bandwidth):
+    for index in range(0, len(data), bandwidth):
+        writer.write(data[index:index + bandwidth])
+        await asyncio.sleep(1)
+        await writer.drain()
